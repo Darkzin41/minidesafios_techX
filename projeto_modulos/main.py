30e1ac32d6a88fd26421a461bb34b5e08587ca0f
Ariel Rabelo <arielasafe09@gmail.com>
2026-09-07 23:23:17 -0300

    adicionando atividade de modulo de calculadora.py em um novo diretorio para entrega da atividade front end

---
+++ projeto_modulos/main.py
@@ -0,0 +1,7 @@
+import calculadora
+
+s = calculadora.somar(10,5)
+sub = calculadora.subtrair(10,5)
+mult = calculadora.multiplicar(10,5)
+dividir = calculadora.dividir_numeros(10,5)
+print(f"Soma: {s}, \nSubtração: {sub}, \nMultiplicação: {mult}, \nDivisão: {dividir}")
